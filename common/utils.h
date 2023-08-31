4aa18a44746287252f26caa334e2f59353ab20e9
longfu <haoshuai9291@163.com>
2023-08-31 13:48:13 +0800

    日志配置

--- common/utils.h
+++ common/utils.h
@@ -8,13 +8,15 @@ extern volatile sig_atomic_t run;
 extern "C" void SignalHandle(const char *data, size_t size);
 
 #define LOG_INIT(X, Y)                     \
+    FLAGS_colorlogtostdout = 1;            \
     FLAGS_colorlogtostderr = 1;            \
     FLAGS_alsologtostderr = 1;             \
     FLAGS_logbufsecs = 0;                  \
-    FLAGS_max_log_size = 1024;             \
+    FLAGS_max_log_size = 512;             \
     FLAGS_log_dir = Y;                     \
     google::InitGoogleLogging(X);          \
     google::InstallFailureSignalHandler(); \
+    google::EnableLogCleaner(30);          \
     google::InstallFailureWriter(&SignalHandle)
 
 #define LOG_DEINIT() \
